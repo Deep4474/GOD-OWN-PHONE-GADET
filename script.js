48d6c7cc1ef8d27495901916294a2b6a6b31997e
Deep4474 <deepword37@gmail.com>
2025-07-30 14:44:34 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // API base URL
-const API_BASE_URL = '';
+const API_BASE_URL = 'https://phone-2cv4.onrender.com';
 const API_ENDPOINTS = {
   REGISTER: '/api/auth/register',
   LOGIN: '/api/auth/login',
